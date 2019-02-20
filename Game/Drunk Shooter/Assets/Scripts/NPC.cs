1271268901314e7fb6754fcb1ceb417a70d9ea85
Andreea Mazarianu <s188697@uos.ac.uk>
2019-02-20 13:27:21 +0000

    added reticle and enemy looking
    started changing reticle to render in front of everything

---
+++ Game/Drunk Shooter/Assets/Scripts/NPC.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour {
+    
+	void LateUpdate () {
+        Vector3 cameraPos = Camera.main.transform.position;
+        cameraPos.y = transform.position.y; //Look at Camera on all axis except Y axis
+        transform.LookAt(cameraPos);
+	}
+}
---
+++ Game/Drunk Shooter/Assets/Scripts/reticleControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class reticleControl : MonoBehaviour {
+
+    [SerializeField]
+    Transform[] waypoints;
+
+    [SerializeField]
+    float reticleMovementSpeed = 2.5f;
+
+    int waypointIndex = 0;
+
+    void Start()
+    {
+        
+        transform.position = waypoints[waypointIndex].transform.position;
+    }
+
+    void Update()
+    {
+        MoveReticle();
+    }
+
+    void MoveReticle()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[waypointIndex].transform.position, reticleMovementSpeed * Time.deltaTime);
+        if (transform.position == waypoints[waypointIndex].transform.position)
+            waypointIndex += 1;
+        if (waypointIndex == waypoints.Length)
+            waypointIndex = 0;
+    }
+}
