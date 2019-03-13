1b33a0a9296c8861c02de4419afc8c6d1a3ef08d
Dawid Hojka <36009907+Quaximi@users.noreply.github.com>
2019-03-13 14:45:46 +0000

    Added the death animations in.
    In case something goes wrong I uploaded a separate version of the game project.

---
+++ Research/Dawid/Drunk Shooter/Assets/Scripts/NPC.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour {
+    
+	void LateUpdate () //Update after all the Update() functions have done, hence "LateUpdate()"
+    {
+        Vector3 cameraPos = Camera.main.transform.position; //Save the camera's position to the variable "cameraPos"
+        cameraPos.y = transform.position.y; //Look at Camera on all axis except Y axis (unnecessary for Y axis, and will look weird)
+        transform.LookAt(cameraPos); //Look at the Camera
+	}
+}
