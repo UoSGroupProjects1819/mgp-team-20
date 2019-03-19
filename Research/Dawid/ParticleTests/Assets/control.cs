00c7fad56816af9d0228c282558765ed865642a2
Dawid Hojka <36009907+Quaximi@users.noreply.github.com>
2019-03-19 16:37:41 +0000

    Added the Unity project I used to test how particles work.

---
+++ Research/Dawid/ParticleTests/Assets/control.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class control : MonoBehaviour
+{
+    public GameObject particle;
+    void Update()
+    {
+        var mousePos = Input.mousePosition;
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            particle.transform.position = Camera.main.ScreenToWorldPoint(mousePos);
+        }
+
+    }
+
+}
