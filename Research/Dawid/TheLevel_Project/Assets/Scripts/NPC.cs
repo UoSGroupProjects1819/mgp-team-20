18c65c098f636d19d1931f5677f3e8a1932f2590
Dawid Hojka <36009907+Quaximi@users.noreply.github.com>
2019-03-27 14:43:24 +0000

    Work on implementing the level into Unity.
    I have managed to finish the bar area of the level, and will soon move to the casino and strip club area.

---
+++ Research/Dawid/TheLevel_Project/Assets/Scripts/NPC.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour {
+
+    public float deathTimer;
+    public bool deathActive;
+    public ParticleSystem onKillEffect;
+
+    public void KillEnemy()
+    {
+        Instantiate(onKillEffect, gameObject.transform.position, Quaternion.identity);
+        gameObject.SetActive(false); //Disable the object - Destroying the object is too slow of a process to do
+
+    }
+
+    private void Update()
+    {
+        if (deathActive && deathTimer > 0)
+        {
+            deathTimer -= Time.deltaTime;
+        }
+
+        if(deathActive && deathTimer <= 0)
+        {
+            KillEnemy();
+        }
+
+    }
+
+    void LateUpdate () //Update after all the Update() functions have done, hence "LateUpdate()"
+    {
+        Vector3 cameraPos = Camera.main.transform.position; //Save the camera's position to the variable "cameraPos"
+        cameraPos.y = transform.position.y; //Look at Camera on all axis except Y axis (unnecessary for Y axis, and will look weird)
+        transform.LookAt(cameraPos); //Look at the Camera
+	}
+}
---
+++ Research/Dawid/TheLevel_Project/Assets/Scripts/effect.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class effect : MonoBehaviour
+{
+
+    private ParticleSystem thisEffect;
+
+    public void Start()
+    {
+        thisEffect = GetComponent<ParticleSystem>();
+    }
+
+    public void Update()
+    {
+        if (!thisEffect.IsAlive())
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void LateUpdate() //Update after all the Update() functions have done, hence "LateUpdate()"
+    {
+        Vector3 cameraPos = Camera.main.transform.position; //Save the camera's position to the variable "cameraPos"
+        cameraPos.y = transform.position.y; //Look at Camera on all axis except Y axis (unnecessary for Y axis, and will look weird)
+        transform.LookAt(cameraPos); //Look at the Camera
+    }
+
+
+}
