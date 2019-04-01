592e57678187c62c93b20db63c514daf398acffc
XHyperNovaX <anthonyguyskipwith@gmail.com>
2019-04-01 10:50:23 +0100

    moving code to the new map

--- Research/Dawid/TheLevel_Project/Assets/Scripts/NPC.cs
+++ Research/Dawid/TheLevel_Project/Assets/Scripts/NPC.cs
@@ -2,37 +2,92 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NPC : MonoBehaviour {
+public class NPC : MonoBehaviour
+{
+
+    //public Vector3[] spawnLocations = { new Vector3(-4.5f, -0.9f, 2.9f),
+    //new Vector3(3.7f, -0.9f, 2.9f),
+    //new Vector3(-0.5f, -0.9f, -0.6f),
+    //new Vector3(4.7f, -0.9f, -4.5f),
+    //new Vector3(-4.2f, -0.9f, -8.6f),
+    //new Vector3(0.2f, -0.9f, -10.0f) };
+
+    public GameObject player;
 
     public float deathTimer;
     public bool deathActive;
     public ParticleSystem onKillEffect;
+    public float aggro;
 
-    public void KillEnemy()
+    private float movementSpeed = 1.0f;
+    private float attackCooldown;
+    private float lastShotNPC = -1.0f;
+
+    private float hitChance = 5;
+    private float random = 0;
+    private int spawnLocationChoice;
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        aggro = player.GetComponent<Movement>().lastShot;
+    }
+
+    void KillEnemy()
     {
         Instantiate(onKillEffect, gameObject.transform.position, Quaternion.identity);
-        gameObject.SetActive(false); //Disable the object - Destroying the object is too slow of a process to do
-
+        switch (spawnLocationChoice)
+        {
+            case 0:
+                gameObject.transform.position = new Vector3(-4.5f, -0.9f, 2.9f);
+                break;
+            case 1:
+                gameObject.transform.position = new Vector3(3.7f, -0.9f, 2.9f);
+                break;
+            case 2:
+                gameObject.transform.position = new Vector3(-0.5f, -0.9f, -0.6f);
+                break;
+            case 3:
+                gameObject.transform.position = new Vector3(4.7f, -0.9f, -8.6f);
+                break;
+            case 4:
+                gameObject.transform.position = new Vector3(-4.2f, -0.9f, -8.6f);
+                break;
+            case 5:
+                gameObject.transform.position = new Vector3(0.2f, -0.9f, -10.0f);
+                break;
+        }
+        deathActive = false;
+        gameObject.SetActive(false);
     }
 
-    private void Update()
+    void Update()
     {
+        spawnLocationChoice = Random.Range(0, 5);
         if (deathActive && deathTimer > 0)
-        {
             deathTimer -= Time.deltaTime;
-        }
-
-        if(deathActive && deathTimer <= 0)
-        {
+        if (deathActive && deathTimer <= 0)
             KillEnemy();
-        }
-
+        if (aggro < 0.0f)
+            lastShotNPC = Time.time;
     }
 
-    void LateUpdate () //Update after all the Update() functions have done, hence "LateUpdate()"
+    void LateUpdate() //Update after all the Update() functions have done, hence "LateUpdate()"
     {
         Vector3 cameraPos = Camera.main.transform.position; //Save the camera's position to the variable "cameraPos"
         cameraPos.y = transform.position.y; //Look at Camera on all axis except Y axis (unnecessary for Y axis, and will look weird)
         transform.LookAt(cameraPos); //Look at the Camera
-	}
+        if (aggro > 0.0f)
+        {
+            //Aggro onto the player only after they have taken their first shot of the game
+            attackCooldown = Random.Range(2, 4);
+            if (Time.time - lastShotNPC >= attackCooldown)
+            {
+                //Shoot the player
+                random = Random.Range(1, 10); // 4/10 hit chance
+                if (random > hitChance)
+                    player.GetComponent<Movement>().health -= 1.0f;
+            }
+        }
+    }
 }
--- Research/Dawid/TheLevel_Project/Assets/Scripts/movement.cs
+++ Research/Dawid/TheLevel_Project/Assets/Scripts/movement.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class movement : MonoBehaviour
+public class Movement : MonoBehaviour
 {
     public GameObject player; //Used for shortening code - I can type "player" instead of GameObject.Find("player") etc
     public GameObject pistol;
@@ -12,8 +12,9 @@ public class movement : MonoBehaviour
     public GameObject shotgun;
 
     public float lastShot = -1.0f; //The time since the last shot, used for cooldowns. Default at -1.0f so the enemies don't aggro instantly
-    //In the NPC file I will make it so they check if lastShot is higher than 0, meaning the player has shot, and then they can aggro
+                                   //In the NPC file I will make it so they check if lastShot is higher than 0, meaning the player has shot, and then they can aggro
 
+    public float health = 100.0f;
     public float speed = 7.5f; //Movement speed of the player
     public float jumpSpeed = 12.5f; //Jump speed of the player
     public float gravity = 20.0f; //Gravity speed
@@ -65,6 +66,8 @@ public class movement : MonoBehaviour
 
     void Update()
     {
+        if (health <= 0.0f)
+            Application.Quit();
         var heading = reticleChild.transform.position - cameraTransform.position; //Calculation of the angle from the Camera to the reticle
         var distance = heading.magnitude; //^
         var direction = heading / distance; //^
