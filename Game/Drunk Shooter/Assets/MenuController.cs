b46ed7c628e17f56de1e61b7f59a6a1dbb7ff773
Dawid Hojka <36009907+Quaximi@users.noreply.github.com>
2019-05-06 22:03:42 +0100

    Added a start menu and finished level.

---
+++ Game/Drunk Shooter/Assets/MenuController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+
+    public Button startButton;
+    public Button exitButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startButton.onClick.AddListener(delegate { SceneManager.LoadScene(sceneName: "Level_Bar");
+        });
+
+        exitButton.onClick.AddListener(delegate {
+            Application.Quit();
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
